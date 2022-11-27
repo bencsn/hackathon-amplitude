288387f4ee854c64bc39bc9b2ffc279fef46d4b3
1994benc <43471788+1994benc@users.noreply.github.com>
2022-11-27 13:09:22 +0000

    Accept multiple phrases

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI, HTTPException, Response, JSONResponse
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 import torch
@@ -33,32 +33,38 @@ pipe = pipe.to("cuda")
 def home():
     return {"message": "The service is running"}
 
-
 class Data(BaseModel):
-    prompt: str
-
+  input: list
 
 @app.post("/analyse")
 def analyse_data(data: Data):
-    if not data.prompt:
-        raise HTTPException(status_code=400, detail="Prompt is required")
-    image = pipe(data.prompt, height=768, width=768).images[0]
+    if not data.input:
+        raise HTTPException(status_code=400, detail="Input is required")
+
+    # loop through the input and generate the images
+    images = []
+    for prompt in data.input:
+        image = pipe(prompt['phrase'], height=768, width=768).images[0]
+        images.append({
+            "id": prompt["id"],
+            "image": image
+        })
+    # image = pipe(data.prompt, height=768, width=768).images[0]
     # delete folder images and all its contents
-    if os.path.exists("images"):
-        for file in os.listdir("images"):
-            os.remove(os.path.join("images", file))
-        os.rmdir("images")
-
     os.makedirs("images", exist_ok=True)
-    image.save("images/image.png")
-    return {"prompt": data.prompt, "success": True}
 
 
-@app.get("/image")
-def serve_image():
-    if os.path.exists("images/image.png"):
+    for count, image in enumerate(images):
+        # save the image to the images folder
+        image["image"].save(f"images/{image['id']}.png")
+    return {"success": True}
+
+# an endpoint that accepts url param as image id and returns the image
+@app.get("/image/{image_id}")
+def serve_image(image_id: str):
+    if os.path.exists(f"images/{image_id}.png"):
         image_bytes = None
-        with open("images/image.png", "rb") as f:
+        with open(f"images/{image_id}.png", "rb") as f:
             image_bytes = f.read()
         return Response(content=image_bytes, media_type="image/png")
     else:
