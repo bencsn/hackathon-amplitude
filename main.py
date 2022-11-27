e8b250e3761e9737ee1591000cf91675173452e1
1994benc <43471788+1994benc@users.noreply.github.com>
2022-11-27 10:20:32 +0000

    Correct the code

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 import torch
 import os
+import base64
 import numpy as np
 
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
 app = FastAPI()
 
-
 # Setup the StableDifussion model. This will take a while to load so we do it once here and reuse the model
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(
-    model_id, torch_dtype=torch.float16)
+model_id = "stabilityai/stable-diffusion-2"
+
+# Use the Euler scheduler here instead
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
 
 
 @app.get("/")
@@ -31,16 +33,18 @@ def analyse_data(data: Data):
         raise HTTPException(status_code=400, detail="Prompt is required")
     image = pipe(data.prompt, height=768, width=768).images[0]
     # Make a directory called "images" and save the image there
+    os.makedirs("images", exist_ok=True)
     image.save("images/image.png")
     return {"prompt": data.prompt, "success": True}
 
 
 @app.get("/image")
 def serve_image():
-    # Check if the image exists
     if os.path.exists("images/image.png"):
-        with open("images/image.png", "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
-        return encoded_string
+        image_bytes = None
+        with open("images/image.png", "rb") as f:
+            image_bytes = f.read()
+        return Response(content=image_bytes, media_type="image/png")
     else:
         raise HTTPException(status_code=404, detail="Image not found")
+
